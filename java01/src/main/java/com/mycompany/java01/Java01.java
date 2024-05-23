9952a829e5073351bbf7b47a289ecabd2550d8f7
Sidney Ibelmão <ibelmao@gmail.com>
2024-05-23 09:30:36 -0300

    criei o primeiro arquivo

---
+++ java01/src/main/java/com/mycompany/java01/Java01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java01;
+
+/**
+ *
+ * @author sidney
+ */
+public class Java01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
